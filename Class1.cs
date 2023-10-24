ca132f7f9cf44ca7013c9984b238f6a062682f74
15084 <15084@LAPTOP-O0VP2NQS>
2023-10-24 11:41:50 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using AimRobot.Api;
+using AimRobot.Api.plugin;
+
+namespace ar_example_plugin {
+    public class Class1 : PluginBase {
+
+        public override string GetAuthor() {
+            return "jiamingzhang";
+        }
+
+        public override string GetDescription() {
+            return "kpm限制插件，kpm<2";
+        }
+
+        public override string GetPluginName() {
+            return "ar-example-plugin";
+        }
+
+        public override Version GetVersion() {
+            return new Version(1, 0, 0);
+        }
+
+        public override void OnDisable() {
+            
+        }
+
+        public override void OnEnable() {
+            
+        }
+
+        public override void OnLoad() {
+            Robot.GetInstance().GetPluginManager().RegisterListener(this, new MyPluginEventListener());
+
+            //这是监听指令的
+           
+        }
+
+    }
+}
---
+++ MyPluginEventListener.cs
@@ -0,0 +1,49 @@
+﻿using AimRobot.Api;
+using AimRobot.Api.events;
+using AimRobot.Api.events.ev;
+using System;
+using System.Collections.Generic;
+
+namespace ar_example_plugin
+{
+    public class MyPluginEventListener : IEventListener
+    {
+        // 使用成员变量来代替高频率出现的局部变量[^1^][1]
+        private Dictionary<string, int> killsPerMinute = new Dictionary<string, int>();
+
+        // 定义一个常量表示每分钟允许的最大杀敌数
+        private const int MAX_KILLS_PER_MINUTE = 2;
+
+        // 定义一个DateTime变量来存储上次检查时间
+        private DateTime lastCheckTime = DateTime.UtcNow;
+
+        [AimRobot.Api.events.EventHandler]
+        public void whenPlayerDeathEvent(PlayerDeathEvent eventArgs)
+        {
+            // 获取杀死对方的玩家的名字
+            string playerName = eventArgs.killerName;
+
+            // 如果玩家不在字典中，添加他们并赋值为0
+            if (!killsPerMinute.ContainsKey(playerName))
+            {
+                killsPerMinute[playerName] = 0;
+            }
+
+            // 增加玩家的杀敌数
+            killsPerMinute[playerName]++;
+
+            // 计算上次检查以来经过的秒数
+            int secondsSinceLastCheck = (int)(DateTime.UtcNow - lastCheckTime).TotalSeconds;
+
+            // 检查玩家是否超过了每分钟允许的最大杀敌数
+            if (killsPerMinute[playerName] / secondsSinceLastCheck > MAX_KILLS_PER_MINUTE)
+            {
+                // 踢出玩家
+                Robot.GetInstance().KickPlayer(eventArgs.killerName, "你已经超过了每分钟允许的最大杀敌数2，kpm<2");
+            }
+
+            // 更新上次检查时间
+            lastCheckTime = DateTime.UtcNow;
+        }
+    }
+}
